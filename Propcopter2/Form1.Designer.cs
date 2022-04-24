a472a836e15b807a386c1670ccb4d33dbb3bd33e
Luke Dale <lhdale@crimson.ua.edu>
2022-04-24 16:18:33 -0500

    Add project files.

---
+++ Propcopter2/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Propcopter2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Kloud = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kloud)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("OCR A Extended", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(342, -1);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(84, 83);
+            this.Label1.TabIndex = 4;
+            this.Label1.Text = "0";
+            this.Label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Propcopter2.Properties.Resources.GameOver;
+            this.pictureBox6.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(678, 765);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Propcopter2.Properties.Resources.brick2;
+            this.pictureBox5.Location = new System.Drawing.Point(0, -41);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(317, 59);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Propcopter2.Properties.Resources.brick2;
+            this.pictureBox4.Location = new System.Drawing.Point(518, -41);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(160, 59);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Propcopter2.Properties.Resources.Currency2;
+            this.pictureBox3.Location = new System.Drawing.Point(277, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(59, 59);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Propcopter2.Properties.Resources.brick2;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 406);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(241, 59);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Propcopter2.Properties.Resources.brick2;
+            this.pictureBox1.Location = new System.Drawing.Point(437, 406);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Kloud
+            // 
+            this.Kloud.Image = global::Propcopter2.Properties.Resources.Kloud2;
+            this.Kloud.Location = new System.Drawing.Point(291, 567);
+            this.Kloud.Name = "Kloud";
+            this.Kloud.Size = new System.Drawing.Size(110, 187);
+            this.Kloud.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Kloud.TabIndex = 0;
+            this.Kloud.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(678, 766);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Kloud);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kloud)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Kloud;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
+
---
+++ Propcopter2/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Propcopter2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool goLeft;
+        bool goRight;
+        int move = 10;
+        int goUp = 4;
+        int time;
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.D)
+            {
+                goRight = false;
+            }
+            else if(e.KeyCode == Keys.A)
+            {
+                goLeft = false;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.D)
+            {
+                goRight = true;
+            }
+            else if(e.KeyCode == Keys.A)
+            {
+                goLeft = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(10, 100);
+
+            Random rndTwo = new Random();
+            int numTwo = rndTwo.Next(10, 100);
+
+            Random rndThree = new Random();
+            int numThree = rndThree.Next(2);
+
+            Random rndFour = new Random();
+            int numFour = rndFour.Next(2);
+
+            if (goLeft == true && Kloud.Left > 0)
+            {
+                Kloud.Left -= move;
+            }
+
+            if (goRight == true && Kloud.Left < 569)
+            {
+                Kloud.Left += move;
+            }
+
+            pictureBox1.Top += goUp;
+            pictureBox2.Top += goUp;
+            pictureBox4.Top += goUp;
+            pictureBox5.Top += goUp;
+
+            if (pictureBox1.Top > 600)
+            {
+                time++;
+                Label1.Text = time.ToString();
+
+                pictureBox1.Top = 50;
+                pictureBox2.Top = 50;
+
+                if(pictureBox1.Width > (num+10) && numThree == 0)
+                {
+                    pictureBox2.Width += num;
+                    pictureBox1.Width -= num;
+                    pictureBox1.Left += num;
+                }
+                else if(pictureBox2.Width > (num+10) && numThree == 1)
+                {
+                    pictureBox2.Width -= num;
+                    pictureBox1.Width += num;
+                    pictureBox1.Left -= num;
+                }
+                
+            }
+
+            if(pictureBox4.Top > 600)
+            {
+                time++;
+                Label1.Text = time.ToString();
+                pictureBox4.Top = 50;
+                pictureBox5.Top = 50;
+
+                if(pictureBox4.Width > (num+10) && numFour == 0)
+                {
+                    pictureBox5.Width += numTwo;
+                    pictureBox4.Width -= numTwo;
+                    pictureBox4.Left += numTwo;
+                }
+                else if(pictureBox5.Width > (num+10) && numFour == 1)
+                {
+                    pictureBox5.Width -= numTwo;
+                    pictureBox4.Width += numTwo;
+                    pictureBox4.Left -= numTwo;
+                }
+                
+            }
+
+            
+
+            if(Kloud.Bounds.IntersectsWith(pictureBox1.Bounds))
+            {
+                pictureBox6.Visible = true;
+                timer1.Stop();
+            }
+            else if(Kloud.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                pictureBox6.Visible = true;
+                timer1.Stop();
+            }
+            else if(Kloud.Bounds.IntersectsWith(pictureBox4.Bounds))
+            {
+                pictureBox6.Visible = true;
+                timer1.Stop();
+            }
+            else if(Kloud.Bounds.IntersectsWith(pictureBox5.Bounds))
+            {
+                pictureBox6.Visible = true;
+                timer1.Stop();
+            }
+        }
+    }
+}
